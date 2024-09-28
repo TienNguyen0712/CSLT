626711136819a8f846f7922d32c36d62eb944382
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2024-09-28 08:27:56 +0700

    Update BaiTapChuong3.cpp

--- BaiTap/BaiTapChuong3.cpp
+++ BaiTap/BaiTapChuong3.cpp
@@ -1,9 +1,84 @@
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
+#include <vector>
 
 using namespace std;
+long long giathua(int a){
+    if(a == 0 || a == 1 ) {
+        return 1;
+    } else {
+        return a * giathua(a-1);
+    }
+}
+long long C(int a, int b){
+    return giathua(a) / (giathua(b) - giathua(a-b));
+}
 
+int dq1(int n){
+    if (n < 3) return 1;
+    vector<int>x(n + 1);
+    x{1} = 1;
+    x[2] = 1;
+    for (int i = 3; i < n; i++)
+    {
+        x[i] = x[i -1] + (i-1) * x[i -2];
+    }
+    return x[n];
+}
+int dq(int a){
+    if(a < 3){
+        return 1;
+    } return dq(a-1) + (a-1)*dq(a-2);
+}
+int tinhtong(int a){
+    int sum = 0;
+    if(a == 0){
+        return 0; 
+    }
+    int i = a;
+    while (i != 0)
+    {
+        sum = sum + 1/i;
+        i--;
+    }
+    return float(sum);   
+}
+int ucln(int a, int b){
+    int max;
+    if (a == b)
+    {
+        max = a + b;
+    } 
+    a > b ? max = a - b : max = b - a;
+        return max;
+}
+int tong(int a){
+    if(a != 0){
+        return tong(a - 1) + a;
+    } return 0;
+}
+
+int luythua(int a, int b){
+    if(b == 1){
+        return a;
+    }
+    return luythua(a, b-1)*a;
+}
+int isP(int a){
+    if(a < 2){
+        return 0;
+    }
+    int sq = sqrt(a);
+    int j;
+    for ( j = 2; j < sq; j++)
+    {
+        if(a % j){
+            return 0;
+        }
+    }
+    return 1;
+}
 int hinhvuong (){
         int a;
         cout<<"Nhap canh hinh vuong: "; cin>>a;
@@ -284,7 +359,7 @@ int main (){
     cout<<"\nSo Fibbonacci nho hon n: ";
     cout<<"\nSo Armstrong nho hon n: ";
     for (int i = 1; i <= n; i++){
-        sochinhphuong(i);
+        sochinhphuong(i, n);
         sohoanchinh(i);
         songuyento(i);
         sodoixung(i);
@@ -323,4 +398,61 @@ int main (){
         return 0;
     }
     system("cls");
+    //BAI TAP 3.4
+    //CAU A
+    int n, dem = 0;
+    cout<<"Nhap n: "; cin>>n;
+    cout<<n<< "so nguyen to dau tien";
+    int i = 2;
+    while (dem < n)
+    {
+        if(isP(i)){
+            cout<<i<<" ";
+            dem++;
+        }
+        i++;
+    }
+    
+    return 0;
+    //CAU B
+    //Tham Khảo thêm
+    //BAI TAP 3.5
+    //CAU A
+    int a, b;
+    cout<<"Nhap co so: "; cin>>a;
+    cout<<"Nhap so mu: "; cin>>b;
+    cout<<"Kết quả: "<<luythua(a,b);
+    return 0;
+    //CAU B
+        int a, b;
+    cout<<"Nhap co so: "; cin>>a;
+    cout<<"Nhap so mu: "; cin>>b;
+    cout<<"Ket qua: "<<ucln(a, b);
+    return 0l
+    //CAU C
+    int n;
+    cout<<"Nhap n: "; cin>>n;
+    cout<<"Ket qua: "<<tong(n);
+    return 0;
+    //CAU D Tham khảo thêm
+    //CAU C Sửa thêm code
+    cout<<fixed<<setprecision(2)<<float(tinhtong(2));
+    return 0;
+    //BAI TAP 3.6
+    //CAU A X7 = 135
+    //CAU B 
+    int n;
+    cout<<"Nhap n: "; cin>>n;
+    cout<<dq(n);
+    return 0;
+    //CAU C
+    int n;
+    cout<<"Mhap n: "; cin>>n;
+    cout<<dq1(n);
+    //BAI TSP 3.9
+    int n, k;
+    cout<<"Nhap n: "; cin>>n;
+    cout<<"Nhap k: "; cin>>k;
+    cout<<"To hop: "<<C(n, k);
+    return 0;
 }
