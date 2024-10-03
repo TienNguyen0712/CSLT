8e826b67628bf54927540840d480026f5dc6fc3f
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2024-10-03 09:15:19 +0700

    Update BaiTapChuong4.cpp

--- BaiTap/BaiTapChuong4.cpp
+++ BaiTap/BaiTapChuong4.cpp
@@ -11,35 +11,48 @@ void tinhtong(int a[], int &n, int i){
     }
     cout<<"Tong cua cac chu so cua so trong mang la: "<<s<<" ";
 }
-int sont(int n){
+bool sont(int n){
+    if (n < 2) return false;
     int d = 0;
-    if(n < 2){return 0;}
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i <= sqrt(n) + 1; i++)
     {
         if(n % i == 0) d++;
-        if(d == 2) return 1;
     }
+    if(d == 2) return true;
 }
-int socp(int n){
-    for (int i = 1; i <= n; i++)
-    {
-        if(i*i == n) return 1;
+bool sohc(int n){
+    int sum = 1;
+    for (int i = 2; i * i <= n; i++){
+        if (n % i == 0) {
+            sum += i + (n / i);
+        }
     }
-    return 0;
+    return sum == n && n != 1;
 }
-int sohc(int n){
-    int sum = 0;
-    for (int i = 1; i < n; i++){
-        sum = sum + i;
-    }
-    if(sum == n) return 1;
+bool socp(int n){
+    int x = sqrt(n);
+    return  x*x == n;
 }
+void hoanvi(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void sapxep(int a[], int &n){
+    for (int i = 0; i < n; i++)
+    for (int j = i + 1; j  < n; j++)
+    if(a[i] < a[j])
+        hoanvi(a[i], a[j]);
+}
+void
 int main(){
     //BAI TAP 4.1
     int n;
     cout<<"Nhap so co trong mang: "; cin>>n;
+    int t;
+    cout<<"Nhap so lon thu k trong mang: "; cin>>t;
     int a[n];
-    int s= 0, demnt = 0, demcp = 0, demhc = 0;
+    int s = 0, demnt = 0, demcp = 0, demhc = 0;
     for (int i = 0 ; i < n; i++)
     {
         cin>>a[i];
@@ -52,8 +65,14 @@ int main(){
         if(sont(a[i])) demnt++;
         if(socp(a[i])) demcp++;
         if(sohc(a[i])) demhc++;
+        //CAU D
+        sapxep(a, n);
+        cout<<a[t];
+        //CAU E
+        if(sont(a[i])) hoanvi()
     }
-    cout<<"Trung binh cong cua cac so trong mang: "<<s<<"\n";
+
+    cout<<"Trung binh cong cua cac so trong mang: "<<fixed<<setprecision(2)<<float(s/n)<<"\n";
     cout<<"Co "<<demnt<<" so nguyen to trong mang\n";
     cout<<"Co "<<demcp<<" so chinh phuong trong mang\n";
     cout<<"Co "<<demhc<<" so hoan chinh trong mang\n";
