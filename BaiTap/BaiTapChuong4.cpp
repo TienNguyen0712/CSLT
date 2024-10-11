69c4f0b8b588ece5c199be6ef5d9034e9ced356e
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2024-10-11 08:07:47 +0700

    Update BaiTapChuong4.cpp

--- BaiTap/BaiTapChuong4.cpp
+++ BaiTap/BaiTapChuong4.cpp
@@ -1,221 +1,85 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> //Thư viện tổng
 using namespace std;
-int tong(int b){
-    //CAU A
-    int i = b, s = 0, d;
-    while(i != 0){
-        s += i%10;
-        i/=10;
+        //Xoa phan tu thu k trong mang
+    void xoaphantu(int a[], int &c, int k){
+        for (int i = k; i < c; i++)
+                a[i] = a[i + 1];
+                c--;
     }
-    return s;
-}
-bool sont(int a){
-    //CAU C
-    int d = 0;
-    if (a < 2) return false;
-    for(int i  = 1; i <= a; i++){
-        if(a % i == 0) d++;
+        //Them phan tu x vao vi tri thu k trong mang
+    void themphantu(int a[], int &d, int k, int x){
+        for (int j = k; i < d; i--)
+            a[i] = a[i - 1];
+            a[k] = x;
+            d++;
     }
-    return d == 2;
-}
-bool socp(int a){
-    //CAU C
-    int i = 0;
-    while(i <= a){
-        if(i * i == a) return a > 0;
-        i++;
+        //Tinh tong theo kieu tra ve
+    int tinhtong(int a[], int &b){
+        int s = 0, i;
+        for (i = 0; i < b; i++) {s  = s + a[i]};
+        return s;
     }
-}
-bool sohc(int a){
-    //CAU C
-    int s = 0;
-    for(int i = 1; i <= sqrt(a); i++){
-        if(a % i == 0){
-            s+=i;
-            if(s/i != i){
-                s+=a/i;
-            }
-        }
-    }
-    return s - a == a;
-}
-int ucln(int a, int b){
-    //CAU G
-    if(b == 0) return a;
-    return ucln(b, a%b);
-}
-void xuat(int a[], int n){
-    //CAU G
-    int d = 0;
-        for(int j = 0; j < n; j++){
-        for(int z = j+1; z < n; z++){
-            if(ucln(a[j], a[z]) == 1) d++;
-        }
-    }
-    cout<<"Cap so nguyen to cung nhau: "<<d;
-}
-void swap(int &a, int &b){
-    //CAU D
-    int temp = a;
-    a = b;
-    b = temp;
-}
-void sapxepmang(int arr[], int n){
-    //CAU D
-    for(int j = 0; j < n; j++){
-        for(int z = j+1; z < n; z++){
-            if(arr[j] < arr[z]) swap(arr[j], arr[z]);
-        }
-    }
-}
-bool sont(int a){
-    //CAU F
-    int d = 0;
-    if(a < 1) return false;
-    for (int i = 1; i <= a; i++)
-    {
-        if(a % i == 0) d++;
-    }
-    return d == 2;
-}
-int sonnguoc(int a){
-    //CAU F
-    int sum = 0;
-    for (int i = a; i != 0; i/=10)
-    {
-       sum = sum*10 + i%10; 
-    }
-    return sont(sum);
-}
-int timmangdainhat(int arr[], int n){
-    //BAI TAP 4.4 CAU A
-    int dp[n];
-    for( int i = 0; i < n; i++){
-        dp[i] = 1;
-    }
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {   
-            int max = arr[j] + 1;
-            if(arr[i] > max) max = arr[i]; 
-        
-    }
-    int re = dp[0];
-    for(int i = 0; i < n; i++){
-        if(dp[i] > re) re = dp[i];
-    }
-    return re;
-    }
-}
-int tinhtong(int a[], int b){
-    //BAI TAP 4.4 CAU B
-    int m, c = 0;
-    cout<<"Nhap tong n muon tim: "; cin>>m;
-    for(int j = 0; j < b; j++){
-        for(int z = j+1; z < b; z++){
-            if(a[j] + a[z] == m) c++;
-        }
-    }
-    return c;
-}
-int cacsokhacnhau(int arr[], int n){
-    //BAI TAP 4.4 CAU D
-    int c[n];
+int main(){
+    //Vidu1 //Nhap cac phan tu
+    int n; cin>>n;
+    int a[n];
     for (int i = 0; i < n; i++)
     {
-        for (int j = i+1; i < n; j++)
-        {
-            if(arr[i] != arr[j]) c[i] = arr[i];            
-        }
+        cin>>a[i];
     }
-    sapxepmang(c, n);
-    return c[0]*c[1]*c[2];
-}
-int giatrikhac(int a[], int b){
-    //BAI TAP 4.4 CAU E
-    int c = 0;
-    for(int j = 0; j < b; j++){
-        for(int z = j+1; z < b; z++){
-            if(a[j] != a[z]) c++;
-        }
+            //Duyet cac phan tu
+    for (int j = 0; j < n; i++)
+    {
+        /*Cau lenh duyet phan tu*/
     }
-    return c;
-}
-int xuathiennhieunhat(int a[], int b){
-    //BAI TAP 4.4 CAU F
-    int m;
-    for(int j = 0; j < b; j++){
-        for(int z = j+1; z < b; z++){
-            if(a[j] == a[z]) m = a[j];
-        }
+            //Tinh tong
+    int s = 0;
+    int n; cout<<"Nhap so phan tu cua mang: "; cin>>n;
+    int b[n];
+    for (int z = 0; z < n; z++)
+    {
+        s = s + b[z];
     }
-    return m;
-}
-int tinhTong(int arr[], int n) {
-    //BAI TAP 4.7 CAU A
-    if (n == 1) return arr[0];
-        return arr[0] + tinhTong(arr + 1, n - 1);
-}
-int main(){
-    int n; 
-    cout<<"Nhap so thanh phan cua mang: ";
-    cin>>n;
-    int k; 
-    cout<<"Nhap so lon thu k cua mang: ";
-    cin>>k;
-    cout<<"Tong la: ";
-    cout<<"So nguyen to trong mang khi dao nguoc cung la so nguyen to: "; 
-    int i = 0, arr[n], sum = 0, dnt = 0, dcp = 0, dhc = 0;
-    while(i  < n){
-        //BAI TAP 4.1
-        cin>>arr[i];
-        cout<<tong(arr[i])<<" "; //CAU A
-        sum+=arr[i]; //CAU B
-        if(sont(arr[i])) dnt++; //CAU C
-        if(socp(arr[i])) dcp++; //CAU C
-        if(sohc(arr[i])) dhc++; //CAU C
-        if(sont(arr[i])){
-            //CAU F
-            if(sonnguoc(arr[i])){
-                cout<<arr[i]<<" ";
-            }
+    cout<<s;
+            //Dem so phan tu thoa dieu kien
+        int dem = 0;
+    int n; cout<<"Nhap so phan tu cua mang: "; cin>>n;
+    int c[n];
+    for (int x = 0; x < n; x++)
+    {
+        /*Cau lenh dieu kien*/
+        dem++;
     }
-        i++;
-    }
-    cout<<"Trung binh cong: "<<(float)sum/n; //CAU B
-    cout<<"So nguyen to co trong mang: "<<dnt; //CAU C
-    cout<<"So chinh phuong co trong mang: "<<dcp; //CAU C
-    cout<<"So hoan chinh co trong mang: "<<dhc; //CAU C
-    cout<<"Nhap so lon thu k: "; cin>>k; //CAU D
-    sapxepmang(arr, n); //CAU D
-    cout<<"So lon thuc"<< k <<" cua mang : "<<arr[k]; //CAU D
-    //CAU E
-
-
-    xuat(arr, n); //CAU G
-    //BAI TAP 4.2
-    //BAI TAP 4.3
-    //BAI TAP 4.4
-    //CAU A
-    cout<<timmangdainhat(arr, n);
-    //CAU B
-    cout<<tinhtong(arr, n)<<endl;
-    //CAU C
-    sapxepmang(arr, n);
-    cout<<arr[0]<<" "<<arr[n-1];
-    //CAU D
-    cout<<"Tich 3 so lon nhat: "<<cacsokhacnhau(arr, n);
-    //CAU E
-    cout<<giatrikhac(arr, n)<<endl;
-    //CAU F
-    cout<<"Phan tu xuat hien nhieu nhat: "<<xuathiennhieunhat(arr, n)<<endl;
-    //BAI TAP 4.5
-    //BAI TAP 4.6
-    //BAI TAP 4.7
-    //cAU A
-    cout<<tinhTong(arr, n);
-    //CAU B
-    
+    cout<<dem;
     return 0;
+    //MANG 2 CHIEU 
+    //Nhap vao mang 2 chieu 
+    int m, n;
+    cout<<"Nhap so hang: "; cin>>m;
+    cout<<"Nhap so cot: "; cin>>n;
+    int arr[n][m];
+    for (int i = 0; i < m; i++)
+    for( int j = 0; j < n; j++)
+        cin>>arr[i][j];
+    //Xuat mang 2 chieu
+    for(int c = 0; c < m; c++){
+        for(int z = 0; z < n; z++)
+            cout<<arr[c][z];
+    }
+    //Tinh tong 
+    int sum = 0;
+     for(int c = 0; c < m; c++){
+        for(int z = 0; z < n; z++)
+            sum+=arr[c][z];
+    }
+    cout<<sum;
+    //Dem phan tu thoa dieu kien
+        int dem = 0;
+     for(int c = 0; c < m; c++){
+        for(int z = 0; z < n; z++)
+            if (/*Dieu kien bai toan*/)
+                dem++;
+    }
+    cout<<dem;
+    
 }
